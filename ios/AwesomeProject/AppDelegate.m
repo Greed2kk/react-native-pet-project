61d53efe8074dbc4d30766354c1c5d5d3566dfa5
Greed2kk <draiver78@mail.ru>
2021-12-21 12:46:13 +0300

    Configure App Center accessibility

--- ios/AwesomeProject/AppDelegate.m
+++ ios/AwesomeProject/AppDelegate.m
@@ -27,6 +27,15 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
+[AppCenterReactNative register];
+[AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+[AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
