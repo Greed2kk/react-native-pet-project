13db7ffae52f53f3cd7a9cb2e78f9d92f6c5361d
Greed2kk <draiver78@mail.ru>
2021-12-23 16:45:51 +0300

    Refactoring android env variables

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,3 +6,6 @@ printf "%s\n" $ENV_WHITELIST
 set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
 printf "\n.env created with contents:\n\n"
 cat .env
+
+touch ./android/app/src/main/assets/appcenter-config.json
+echo "{ \n \"app_secret\":$ANDROID_SECRET \n }" >> "./android/app/src/main/assets/appcenter-config.json"
--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import codePush from 'react-native-code-push';
 
 import MainNavigation from './navigation/MainNavigation';
 import { SafeAreaProvider } from 'react-native-safe-area-view';
 
-const App: () => Node = () => (
-  <SafeAreaProvider>
-    <MainNavigation />
-  </SafeAreaProvider>
-);
-
-export default codePush({
+const codePushOptions = {
+  installMode: codePush.InstallMode.IMMEDIATE,
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-})(App);
+  mandatoryInstallMode: codePush.InstallMode.ON_NEXT_RESTART,
+  updateDialog: false,
+};
+
+const App: () => Node = () => {
+  useEffect(() => {
+    codePush.sync(codePushOptions);
+    codePush.notifyAppReady();
+  });
+
+  return (
+    <SafeAreaProvider>
+      <MainNavigation />
+    </SafeAreaProvider>
+  );
+};
+
+export default codePush(codePushOptions)(App);
