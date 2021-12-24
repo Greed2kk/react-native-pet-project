38d6258de693fc8200705748e589815aa9190d0b
Greed2kk <draiver78@mail.ru>
2021-12-24 12:56:20 +0300

    Push update env variables

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Creates an .env from ENV variables for use with react-native-config
 cd ${APPCENTER_SOURCE_DIRECTORY}
-echo "BASE_URL=${BASE_URL}" > .env
-echo "FAQ_URL=${FAQ_URL}" >> .env
+echo "CODEPUSH_KEY_IOS=${CODEPUSH_KEY_IOS}" > .env
+echo "CODEPUSH_KEY_ANDROID=${CODEPUSH_KEY_ANDROID}" >> .env
+echo "SECRET_IOS=${SECRET_IOS}" >> .env
+echo "SECRET_ANDROID=${SECRET_ANDROID}" >> .env
 
 touch ./android/app/src/main/assets/appcenter-config.json
-echo "{ \n \"app_secret\":$ANDROID_SECRET \n }" >> "./android/app/src/main/assets/appcenter-config.json"
+echo "{ \n \"app_secret\":$SECRET_ANDROID \n }" >> "./android/app/src/main/assets/appcenter-config.json"
